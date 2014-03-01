04f463833aefb48871e9d589e711bcf12330699a
Scott Davis <scott@thirstyhead.com>
2014-03-01 13:46:33 -0700

    wired up file/open and file/quit

--- griffon-app/views/minecraft/MinecraftView.groovy
+++ griffon-app/views/minecraft/MinecraftView.groovy
@@ -1,14 +1,38 @@
 package minecraft
 
-application(title: 'minecraft',
-  preferredSize: [320, 240],
-  pack: true,
-  //location: [50,50],
-  locationByPlatform: true,
-  iconImage:   imageIcon('/griffon-icon-48x48.png').image,
-  iconImages: [imageIcon('/griffon-icon-48x48.png').image,
-               imageIcon('/griffon-icon-32x32.png').image,
-               imageIcon('/griffon-icon-16x16.png').image]) {
-    // add content here
-    label('Content Goes Here') // delete me
+actions {
+   action(id: 'openAction',
+        name: 'Open',
+        mnemonic: 'O',
+        accelerator: shortcut('O'),
+        closure: controller.openFile)
+
+   action(id: 'quitAction',
+      name: 'Quit',
+      mnemonic: 'Q',
+      accelerator: shortcut('Q'),
+      closure: controller.quit)
+}
+
+
+fileChooserWindow = fileChooser()
+options = [title:'GroovyEdit', 
+           size: [480,320],
+           locationByPlatform:true,
+           iconImage: imageIcon('/griffon-icon-48x48.png').image,
+           iconImages: [imageIcon('/griffon-icon-48x48.png').image,
+                        imageIcon('/griffon-icon-32x32.png').image,
+                        imageIcon('/griffon-icon-16x16.png').image]]
+
+fileViewerWindow = application(options) {
+  menuBar {
+    menu('File') {
+      menuItem openAction
+      separator()
+      menuItem quitAction
+    } 
+  }
+
+  borderLayout()
+  tabbedPane(id: 'tabGroup', constraints: CENTER)
 }
